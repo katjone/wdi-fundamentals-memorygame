86dcda9a1315aed941d4b2ffddf1d0349a41868b
Katie Jones <katie@Beauty.local>
2019-03-15 17:36:37 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,4 +6,4 @@ var card_3 = "king";
 var card_4 = "queen";
 //Display the card value
 console.log("User flipped " + card_1);
-console.log("User flipped " + card_3);
+/console.log("User flipped " + card_3);
