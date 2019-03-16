160fc0dc9bce75113866d5bdd6a6e2fc6d4c7afd
Katie Jones <katie@Beauty.local>
2019-03-15 17:34:34 -0700

    first commit of all thememory game files" git status
    done
    quit
    quit()

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log('Up and running!')
+//Cards
+var card_1 = "queen";
+var card_2 = "king";
+var card_3 = "king";
+var card_4 = "queen";
+//Display the card value
+console.log("User flipped " + card_1);
+console.log("User flipped " + card_3);
